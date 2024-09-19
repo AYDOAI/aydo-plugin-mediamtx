83ce54b14f6ac6f4e4754032f92f50153bf28526
Denis Aydo <pythonist001@proton.me>
2024-09-19 15:49:41 +0300

    Add compile

--- compile.sh
+++ compile.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+ARCHIVE="../aydo_plugin_mediamtx_$1.zip"
+
+rm -r -f ./release
+mkdir -p ./release
+mkdir -p ./release/plugins
+
+cp ./setup ./release/plugins
+
+cd ./release
+rm "$ARCHIVE"
+zip -u -r -q "$ARCHIVE" ./plugins
+
+if [ "$2" != "" ]; then
+  echo "Copy to server: $2"
+  scp "$ARCHIVE" "$2"
+fi
